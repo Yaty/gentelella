58ef34146edaf00c6d8db20eba63c4cb4cd79395
Hugo Da Roit <contact@hdaroit.fr>
2018-07-01 14:30:47 +0200

    Add E2E tests

--- test/e2e/e2e.js
+++ test/e2e/e2e.js
@@ -1,10 +1,73 @@
 const {uri} = require('./config');
 
 module.exports = {
-    ['demo'](browser) {
+    ['Page should be initialized'](browser) {
         browser
             .url(uri)
-            .waitForElementVisible('body', 1000)
+            .waitForElementVisible('body')
+            .waitForElementVisible('#app')
+            .waitForElementVisible('#todo')
             .end();
     },
+
+    ['Can add a todo'](browser) {
+
+    },
+
+    ['Can check a todo'](browser) {
+
+    },
+
+    ['Can unckeck a todo'](browser) {
+
+    },
+
+    ['Can delete a todo'](browser) {
+
+    },
+
+    ['Can update a todo'](browser) {
+
+    },
+
+    ['All filter show all to-dos (todo, active, completed)'](browser) {
+
+    },
+
+    ['Active filter show only active to-dos'](browser) {
+
+    },
+
+    ['Completed filter show only completed to-dos'](browser) {
+
+    },
+
+    ['Counter of to-dos is correct'](browser) {
+
+    },
+
+    ['Text is well set'](browser) {
+
+    },
+
+    ['To-dos are persisted in session'](browser) {
+
+    },
+
+    ['Remove button is not always visible'](browser) {
+
+    },
+
+    ['Remove button is visible'](browser) {
+
+    },
+
+    ['Remove button is removing all to-dos'](browser) {
+
+    },
+
+    ['Style is well defined'](browser) {
+
+    }
+
 };
