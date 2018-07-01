b5b1c37bc11bbb246ca024c5b9d1e41e91b2d677
Hugo Da Roit <contact@hdaroit.fr>
2018-07-01 23:09:54 +0200

    Complete first e2e tests

--- test/e2e/e2e.js
+++ test/e2e/e2e.js
@@ -1,31 +1,71 @@
 const {uri} = require('./config');
 
+function add(browser, text) {
+    return browser.setValue('.new-todo', [text, browser.Keys.ENTER]);
+}
+
 module.exports = {
+    after(browser){
+        browser.end();
+    },
+
+    before(browser) {
+        browser.url(uri);
+    },
+
+    afterEach(browser, done) {
+        browser.execute('app.cleanTodo()', done);
+    },
+
     ['Page should be initialized'](browser) {
         browser
-            .url(uri)
             .waitForElementVisible('body')
             .waitForElementVisible('#app')
             .waitForElementVisible('#todo')
-            .end();
     },
 
     ['Can add a todo'](browser) {
+        const text = 'e2e-testing';
 
+        add(browser, text)
+            .elements('css selector','.todo-list', function(result) {
+                browser.assert.equal(result.value.length, 1);
+                browser.elementIdText(result.value[0].ELEMENT, function(result){
+                    browser.assert.equal(result.value, text);
+                });
+            });
     },
 
     ['Can check a todo'](browser) {
+        const text = 'e2e-testing';
 
+        add(browser, text)
+            .click('.todo-list :first-child input[type=checkbox]')
+            .assert.cssClassPresent('.todo-list :first-child', 'completed');
     },
 
-    ['Can unckeck a todo'](browser) {
+    ['Can uncheck a todo'](browser) {
+        const text = 'e2e-testing';
 
+        add(browser, text)
+            .click('.todo-list :first-child input[type=checkbox]')
+            .assert.cssClassPresent('.todo-list :first-child', 'completed')
+            .click('.todo-list :first-child input[type=checkbox]')
+            .assert.cssClassNotPresent('.todo-list :first-child', 'completed');
     },
 
     ['Can delete a todo'](browser) {
+        const text = 'e2e-testing';
+
+        add(browser, text)
+            .execute(function() {
+                document.querySelector('.todo-list :first-child button[class=destroy]').click()
+            })
+            .assert.elementNotPresent('.todo-list :first-child');
 
     },
 
+    /**
     ['Can update a todo'](browser) {
 
     },
@@ -69,5 +109,5 @@ module.exports = {
     ['Style is well defined'](browser) {
 
     }
-
+**/
 };
