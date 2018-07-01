1077d23eabf30a6d02ec0ec6f5f34bc3a8c3b2b6
Hugo Da Roit <contact@hdaroit.fr>
2018-07-01 14:34:41 +0200

    Add unit tests

---
+++ test/unit/todoapp.js
@@ -0,0 +1,16 @@
+describe('TODO List', () => {
+    it('should add', () => {
+    });
+
+    it('should update', () => {
+    });
+
+    it('should remove', () => {
+    });
+
+    it('should mark as completed', () => {
+    });
+
+    it('should remove all', () => {
+    });
+});
